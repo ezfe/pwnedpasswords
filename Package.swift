b21929267aea3b0938a86c1359c5e48752e93e12
Ezekiel Elin <ezekielelin@me.com>
2022-09-08 16:50:42 -0400

    untested async/await

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "pwnedpasswords",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "PwnedPasswords", targets: ["PwnedPasswords"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.29.1"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.2")
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
     ],
     targets: [
         .target(name: "PwnedPasswords", dependencies: [
-            .product(name: "Vapor", package: "vapor"),
             .product(name: "Crypto", package: "swift-crypto"),
         ])
     ]
--- Sources/PwnedPasswords/PwnedPasswords.swift
+++ Sources/PwnedPasswords/PwnedPasswords.swift
@@ -1,81 +1,66 @@
 import Foundation
-import Vapor
 import Crypto
 
 fileprivate extension Data {
-    func hexEncodedString() -> String {
-        return self.map { String(format: "%02hhX", $0) }.joined()
-    }
+	func hexEncodedString() -> String {
+		return self.map { String(format: "%02hhX", $0) }.joined()
+	}
 }
 
 public struct PwnedPasswords {
-    private let client: Client
-    private let eventLoop: EventLoop
-
-    fileprivate init(client: Client, eventLoop: EventLoop) {
-        self.client = client
-        self.eventLoop = eventLoop
-    }
-    
-    public enum PwnedPasswordsError: Error {
-        case dataConversionError
-        case apiResponseParseError
-        case hashError
-    }
-    
-    public func test(password: String) -> EventLoopFuture<Bool> {
-        guard let utf8Data = password.data(using: .utf8) else {
-            return self.eventLoop.makeFailedFuture(PwnedPasswordsError.dataConversionError)
-        }
-        
-        let hash = Data(Crypto.Insecure.SHA1.hash(data: utf8Data)).hexEncodedString()
-
-        return send(hash)
-    }
-    
-    private func send(_ searchHash: String) -> EventLoopFuture<Bool> {
-        guard searchHash.count == 40 else {
-            return self.eventLoop.makeFailedFuture(PwnedPasswordsError.hashError)
-        }
-
-        let prefix = searchHash.prefix(5)
-        let suffix = searchHash.suffix(35)
-
-        let url = URI(string: "https://api.pwnedpasswords.com/range/\(prefix)")
-        let headers = HTTPHeaders([("Add-Padding", "true")])
-
-        return self.client.get(url, headers: headers).flatMapThrowing { res in
-            guard let responseLength = res.body?.readableBytes else {
-                throw PwnedPasswordsError.apiResponseParseError
-            }
-
-            guard let result = res.body?.getString(at: 0, length: responseLength) else {
-                throw PwnedPasswordsError.apiResponseParseError
-            }
-
-            let lines = result.split { $0.isNewline }
-            for line in lines {
-                guard let separator = line.firstIndex(of: ":") else {
-                    throw PwnedPasswordsError.apiResponseParseError
-                }
-
-                if line[..<separator] == suffix {
-                    guard let count = Int(line[separator...].dropFirst()) else {
-                        throw PwnedPasswordsError.apiResponseParseError
-                    }
-
-                    return count > 0
-                }
-            }
-
-            return false
-        }
-    }
+	public enum PwnedPasswordsError: Error {
+		case dataConversionError
+		case apiRequestError(String)
+		case apiResponseParseError
+		case hashError
+	}
+	
+	public static func test(password: String) async throws -> Bool {
+		guard let utf8Data = password.data(using: .utf8) else {
+			throw PwnedPasswordsError.dataConversionError
+		}
+		
+		let hash = Data(Crypto.Insecure.SHA1.hash(data: utf8Data)).hexEncodedString()
+		
+		return try await send(hash)
+	}
+	
+	private static func send(_ searchHash: String) async throws -> Bool {
+		guard searchHash.count == 40 else {
+			throw PwnedPasswordsError.hashError
+		}
+		
+		let prefix = searchHash.prefix(5)
+		let suffix = searchHash.suffix(35)
+		
+		guard let url = URL(string: "https://api.pwnedpasswords.com/range/\(prefix)") else {
+			throw PwnedPasswordsError.apiRequestError("Unable to build URL")
+		}
+		var request = URLRequest(url: url)
+		request.setValue("true", forHTTPHeaderField: "Add-Padding")
+		guard let (data, _) = try? await URLSession.shared.data(for: request) else {
+			throw PwnedPasswordsError.apiRequestError("Network request failed")
+		}
+			
+		guard let result = String(data: data, encoding: .utf8) else {
+			throw PwnedPasswordsError.apiResponseParseError
+		}
+			
+		let lines = result.split { $0.isNewline }
+		for line in lines {
+			guard let separator = line.firstIndex(of: ":") else {
+				throw PwnedPasswordsError.apiResponseParseError
+			}
+			
+			if line[..<separator] == suffix {
+				guard let count = Int(line[separator...].dropFirst()) else {
+					throw PwnedPasswordsError.apiResponseParseError
+				}
+				
+				return count > 0
+			}
+		}
+			
+		return false
+	}
 }
-
-public extension Request {
-    var pwnedPasswords: PwnedPasswords {
-        .init(client: self.client, eventLoop: self.eventLoop)
-    }
-}
-
